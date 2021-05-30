703899bde16a45a5976e2f117e4d47577a972fbd
mariohuq <mario.huq@gmail.com>
2021-05-30 18:12:34 +1000

    Removed unused variables, delegated constants to dealer.h

--- header.h
+++ header.h
@@ -16,20 +16,23 @@
 #include <unordered_set>
 #include <unordered_map>
 
+#include "dealer.h"
+
 namespace global {
 	constexpr auto players = 2;
+	constexpr auto suits_count = Dealer::maxSuits;
+	constexpr auto ranks_count = Dealer::maxRanks;
+	constexpr auto total = suits_count * ranks_count; // Total number of cards
 
 	/* Constants for card.h */
-	static const std::array<std::string, 4> suits = { "Clubs", "Spades", "Hearts", "Diamonds" };
-	static const std::array<std::string, 13> ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+	static const std::array<std::string, suits_count> suits = { "Clubs", "Spades", "Hearts", "Diamonds" };
+	static const std::array<std::string, ranks_count> ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
 	static std::unordered_map<std::string, short> iranks = {
 		{"2", 0}, {"3", 1}, {"4", 2}, {"5", 3}, {"6", 4}, {"7", 5}, {"8", 6},
 		{"9", 7}, {"10", 8}, {"Jack", 9}, {"Queen", 10}, {"King", 11}, {"Ace", 12}
 	};
 
-	constexpr auto suits_count = 4;
-	constexpr auto ranks_count = 13;
-	constexpr auto total = suits_count * ranks_count; // Total number of cards
+
 
 	/* Constants for rule.h */
 	constexpr bool attack = true;
@@ -38,7 +41,7 @@ namespace global {
 	/* Constants for situation.h */
 	constexpr auto fullcard = 6;
 	constexpr auto our = true;
-	constexpr auto enemys = false;
+	constexpr auto enemies = false;
 
 	/* Constants for thinker.h */
 	constexpr auto depth = 4;
@@ -60,25 +63,6 @@ class Ranker;
 class iCard;
 class iPlayer;
 class Thinker;
-class Situation;
 class CardManager;
 
 class Counter;
-
-static std::string clubs = global::suits[0];
-static std::string spades = global::suits[1];
-static std::string hearts = global::suits[2];
-static std::string diamonds = global::suits[3];
-static std::string c2 = global::ranks[0];
-static std::string c3 = global::ranks[1];
-static std::string c4 = global::ranks[2];
-static std::string c5 = global::ranks[3];
-static std::string c6 = global::ranks[4];
-static std::string c7 = global::ranks[5];
-static std::string c8 = global::ranks[6];
-static std::string c9 = global::ranks[7];
-static std::string c10 = global::ranks[8];
-static std::string jack = global::ranks[9];
-static std::string queen = global::ranks[10];
-static std::string king = global::ranks[11];
-static std::string ace = global::ranks[12];
