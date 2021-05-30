6fea26f5400d0ed7871222e55eb8637bdb2c2ee9
mariohuq <mario.huq@gmail.com>
2021-05-30 18:38:58 +1000

    Removed redundant specifiers and voids

--- iplayer.h
+++ iplayer.h
@@ -3,7 +3,6 @@
 #include "header.h"
 
 class iPlayer {
-private:
 	int _index; // our index
 	bool _we; // its our player
 	bool _turn; // our turn or not
@@ -15,38 +14,34 @@ private:
 	Counter* counter;
 
 public:
-	int total(void); // return total count of our cards
-	std::vector<iCard*>& desk(void); // return ref for desk cards
-	std::vector<iCard*>& hand(void); // return ref for inhand's cards
-	std::vector<iCard*>& unknown(void); // return ref for unknown cards
+	int total(); // return total count of our cards
+	std::vector<iCard*>& desk(); // return ref for desk cards
+	std::vector<iCard*>& hand(); // return ref for inhand's cards
+	std::vector<iCard*>& unknown(); // return ref for unknown cards
 
-public:
 	iPlayer(bool we, bool turn, bool status, std::string name);
-	bool us(void) const; // return if it's our player
-	bool turn(void) const; // return if it's our turn
-	bool status(void) const; // return status for player
-	int index(void) const; // return index
+	bool us() const; // return if it's our player
+	bool turn() const; // return if it's our turn
+	bool status() const; // return status for player
+	int index() const; // return index
 	std::string name(void) const; // return name
 	void set_index(int);
 	void set_counter(Counter* counter); // reset counter for think
 	void set_analyser(Rule* analyser); // set analyser
 	void set_ranker(Ranker* ranker); // set ranker
 
-public:
-	void grab(void); // grab all cards
-	void replenish(void); // get some cards we dont know
+	void grab(); // grab all cards
+	void replenish(); // get some cards we dont know
 	void get(iCard*); // get one card
 	void hit(iCard*); // hit one card
 	void get(std::string&, std::string&); // get one card
 	void hit(std::string&, std::string&); // hit one card
 
-public:
-	std::vector<iCard*> analysis(void); // analysis we could play
+	std::vector<iCard*> analysis(); // analysis we could play
 	double posibility(const iCard* card); // return for one card's posibility
 	double rank(const iCard* card); // return for one card's rank
 
-public:
-	void transform(void); // takein/out our turn's control
-	void toggle(void); // change our attack/defend mode
+	void transform(); // takein/out our turn's control
+	void toggle(); // change our attack/defend mode
 };
 	
