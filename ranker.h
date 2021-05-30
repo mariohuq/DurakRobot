af2fbb121ec9a0ea96075b5dffeae1df25532673
mariohuq <mario.huq@gmail.com>
2021-05-30 19:08:39 +1000

    refactoring

--- ranker.h
+++ ranker.h
@@ -3,21 +3,24 @@
 #include "header.h"
 
 class Ranker {
-	const iCard* _trump;
+	const iCard* trump_;
 
 public:
+  explicit Ranker(const iCard*);
+
+	// Add weight to the trump card
+	double trump(const iCard* card) const;
+
 	// Scale the function value field to [1,2]
 	static double zip_value(double);
+
 	// Calc absolute rank for card
 	static double absolute(const iCard* card);
-
-	Ranker(const iCard*);
 
 	// Calc for progress coefficient
   // left - number of cards unknown
 	static double progress(size_t left);
-	// Add weight to the trump card
-	double trump(const iCard* card) const;
+
 	// Calc repeat rank for attack and defend
 	static double repeat(iPlayer*, const iCard*);
 };
