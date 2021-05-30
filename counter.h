09cdcfb3b1a0c0afe0e85b9e88d2c5de385fc77f
mariohuq <mario.huq@gmail.com>
2021-05-30 18:37:12 +1000

    Removed redundant specifiers

--- counter.h
+++ counter.h
@@ -3,11 +3,9 @@
 #include "header.h"
 
 class Counter {
-private:
 	std::vector<iCard*> _desk; // cards in desk
 	std::vector<iCard*> _unknown; // card we don't know temporary
 
-private:
 	CardManager* manager;
 	std::array<iPlayer*, global::players> players; // all players
 	std::array<size_t, global::players> count; // count of every one's cards
@@ -20,10 +18,8 @@ public:
 	// Move out one card from a specified vector
 	static void moveOut(std::vector<iCard*>& set, iCard*& target);
 
-public:
 	int join(iPlayer* player); // join a player
 
-public:
 	void grab(iPlayer*);
 	void replenish(iPlayer*, int);
 	void hit(iPlayer*, iCard*);
@@ -31,7 +27,6 @@ public:
 	void hit(iPlayer*, std::string&, std::string&);
 	void get(iPlayer*, std::string&, std::string&);
 
-public:
 	void clear(void); // clear desk
 	std::vector<iCard*>& desk(void); // return desk
 	std::vector<iCard*>& unknown(void); // return unknown
