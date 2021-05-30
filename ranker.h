08d40fc5e0bb898982bbbd1c0a6bc707af882f32
mariohuq <mario.huq@gmail.com>
2021-05-30 19:06:30 +1000

    refactoring

--- ranker.h
+++ ranker.h
@@ -6,7 +6,9 @@ class Ranker {
 	const iCard* _trump;
 
 public:
-	static double zipvalue(double);
+	// Scale the function value field to [1,2]
+	static double zip_value(double);
+	// Calc absolute rank for card
 	static double absolute(const iCard* card);
 
 	Ranker(const iCard*);
@@ -15,8 +17,7 @@ public:
   // left - number of cards unknown
 	static double progress(size_t left);
 	// Add weight to the trump card
-	double trump(const iCard* card);
-
+	double trump(const iCard* card) const;
 	// Calc repeat rank for attack and defend
 	static double repeat(iPlayer*, const iCard*);
 };
